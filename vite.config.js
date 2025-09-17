be378e1f4965d86d495322ab9bdbf08f8813a675
Rodrigo <76666085+RodrigoDSVDF@users.noreply.github.com>
2025-09-16 21:55:16 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/Nxus/', // <<< ADICIONE APENAS ESTA LINHA
+  base: '/Nexus/', // <<< ADICIONE APENAS ESTA LINHA
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
